b99e29283ccc3d021b846a87c47ea9e6cd08a3ab
Alexander Kalabin <74727055+K414BIN@users.noreply.github.com>
2021-01-05 15:42:20 +0300

    Add files via upload
    Я думал в Git уйдет папка, а оказалось Git папки не грузит.

---
+++ AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Программа")]
+[assembly: AssemblyDescription("Программа работает с встроенными настройками.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Отсутствует")]
+[assembly: AssemblyProduct("Урок № 8")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("a221e677-8b26-4f3f-9042-6d44e5d07afb")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.2.1.1")]
+[assembly: AssemblyFileVersion("2.1.1.0")]
+[assembly: NeutralResourcesLanguage("ru-RU")]
